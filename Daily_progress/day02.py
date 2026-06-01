80e2d1eeb84afa2a031f0f19b97322fcef7f93f9
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-01 22:00:40 +0530

    day 2

---
+++ Daily_progress/day02.py
@@ -0,0 +1,71 @@
+# Variables
+
+
+x = float(5)
+y = int(10)
+z = str("hello")
+
+print(x)
+print(y)
+print(z)
+
+
+# It will tell the type of the variable
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Python is Case Sensitive
+
+a = 5
+A = "Hello"
+
+# It will print the value of a and A
+print(a)
+print(A)
+
+
+# Multiple Assignment
+a, b, c = 1, 2.5, "Hello"
+print(a)
+print(b)
+print(c)
+
+
+
+# Assigning multiple variables to the same value
+x = y = z = 10
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+# unpacking a list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x, end=" ")
+print(y, end=" ")
+print(z)
+
+
+
+
+
+
+
+
+x = "hello"
+y = "world"
+
+print(x + " " + y)  # Concatenation of strings
+
+
+
+
+
+
