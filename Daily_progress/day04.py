e31eb6bdac6e7ea3baeda97725f68031762cb3f7
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-07 23:10:45 +0530

    day 4

---
+++ Daily_progress/day04.py
@@ -0,0 +1,60 @@
+#Python Numbers
+
+import random
+
+print(random.randrange(1, 10))
+
+
+
+int_num = 10
+float_num = 3.14
+complex_num = 3j
+
+
+print(type(int_num))      # Output: <class 'int'>
+print(type(float_num))    # Output: <class 'float'>
+print(type(complex_num))  # Output: <class 'complex'>
+
+print("\n")
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+print("\n")
+
+
+
+x = 1.10
+y = 1.0
+z = -35.59
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+
+print("\n")
+
+
+
+
+
+
