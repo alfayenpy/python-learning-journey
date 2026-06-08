41f8e1c04c8eb44fcf087165517eb0b69b1b5635
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-08 23:19:34 +0530

    day 5

---
+++ Daily_progress/day05.py
@@ -0,0 +1,42 @@
+# Python Casting
+
+
+
+
+x = 10
+
+print(str(x))  # Convert integer to string
+
+print(type(str(x)))
+
+
+print("\n")
+
+# Input casting
+
+
+age = int(input("Enter age : "))
+
+
+print(type(age))  # Input is always a string
+
+print("\n")
+
+
+
+
+
+'''
+Inside the editor, complete the following steps:
+Create a variable x with the integer value 1
+Convert x to a float and store it in a
+Convert x to a string and store it in b
+Print a and b       '''
+
+
+
+x = 1
+a = float(x)
+b = str(x)
+print(a)
+print(b)
