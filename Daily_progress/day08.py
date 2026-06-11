a20ee8675ac5bed48571454adfb45b817ddf2ca3
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-11 23:54:25 +0530

    day 8

---
+++ Daily_progress/day08.py
@@ -0,0 +1,55 @@
+# Python Escape Characters
+
+
+# It tells python the next character has a special meaning 
+
+
+
+#    "\n"    - "MEANS NEXT LINE"
+
+
+#    "\t"    - "ADD TAB SPACE"
+
+
+
+
+# Double quote inside quote
+
+"hello my name is \"md alfayen mondal\""        # Format
+
+
+
+
+# Single quote
+
+print('It\'s Python')
+
+
+
+# Backslash
+
+# Use double backslash to get one
+
+print("C:\\Users\\Alfayen")
+
+
+
+# Backspace \b  - Removes one character before it.
+
+print("Helloo\b")
+
+
+
+
+'''
+
+| Escape Character | Meaning      |
+| ---------------- | ------------ |
+| `\n`             | New Line     |
+| `\t`             | Tab Space    |
+| `\"`             | Double Quote |
+| `\'`             | Single Quote |
+| `\\`             | Backslash    |
+| `\b`             | Backspace    |
+
+'''
