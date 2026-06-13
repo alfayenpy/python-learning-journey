cf4613fffc40fba1cfe34c009cae26e71aa2ee6a
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-13 11:43:24 +0530

    day 10

---
+++ Daily_progress/day10.py
@@ -0,0 +1,99 @@
+'''
+Inside the editor, complete the following steps:
+
+Create a variable txt with the value "Hello, World!"
+Print the characters from index 2 to 5 (slicing)
+Print txt converted to upper case
+Create a variable name with the value "Python"
+Use an f-string to print "I love Python" using the name variable
+
+'''
+
+
+
+
+txt = "Hello, World!"
+
+print(txt[2:5])
+
+txt = txt.upper()
+
+Variable = "Python"
+
+print(f"I love {Variable}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Python Booleans
+
+
+'''A Boolean has only two values True or False'''
+
+
+# Using Comparison Operators
+
+num = 10 > 5
+
+print(num)
+
+print(5 == 10)
+
+
+
+
+age = 21
+
+print(age>20)
+
+
+
+
+# Boolean from Strings
+
+
+# Empty String = False
+
+
+print(bool(""))
+
+
+
+# Boolean from Numbers
+
+print(bool(0))
+
+
+
+
+
+'''
+
+Inside the editor, complete the following steps:
+Print the result of 10 > 9
+Print the result of 10 == 9
+Print the result of bool("Hello")
+Print the result of bool(0)
+
+'''
+
+
+
+print(10>9)
+print(10==9)
+print(bool("Hello"))
+print(bool(0))
