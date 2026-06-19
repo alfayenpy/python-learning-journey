853289239a52bb5ad0de00fe138cd3b2dd22ffad
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-19 21:38:45 +0530

    day 13

---
+++ Daily_progress/day13.py
@@ -0,0 +1,35 @@
+# Lists
+
+
+cars = ["lambo", "ferrari", "audi"]
+
+print(cars)
+
+hotel = ["room1", "room2", "room3", 52]  #list can store multiple types
+
+
+# Access List items
+
+# Normal Indexing
+
+print(cars[0])
+print(cars[1])
+
+
+# Negative Indexing
+
+print(cars[-1])
+
+
+
+
+
+
+
+# Access List Items
+
+print(hotel[1:3])
+
+print(cars[-2:])
+
+print(cars[:5])
