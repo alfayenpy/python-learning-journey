4affb891538c7ca05d33a5822810b11fd39affa3
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-20 22:26:06 +0530

    day 14

---
+++ Daily_progress/day14.py
@@ -0,0 +1,39 @@
+# Change List Items
+
+fruits = ["apple", "banana", "guava"]
+
+fruits[0] = "Strawberry"
+
+print(fruits)
+
+
+# Change Multiple items at once
+
+fruits[0:2] = ["milk", "bread"]
+
+
+fruits[1:1] = ["potato"]
+
+print(fruits)
+
+
+
+# Challenge Code
+
+
+countries = ["India", "Japan", "Canada", "Germany"]
+
+countries[1] = "korea"
+
+countries[-1] = "France"
+
+print(countries)
+
+print()
+
+countries = ["India", "Japan", "Canada", "Germany"]
+
+countries[1:3] = ["Korea", "France"]
+
+print(countries)
+
