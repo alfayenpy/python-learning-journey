534618e8a1caf71b692d5b1866390e0ba09b03dc
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-21 23:32:09 +0530

    day 15

---
+++ Daily_progress/day15.py
@@ -0,0 +1,27 @@
+# Add List Items
+
+
+#   append()
+
+# Adds items in the last
+
+items = ["item1", "item2", "item3"]
+
+items.append("item4")
+
+print(items)
+
+
+
+
+
+#   insert()
+
+# Adds item at a specific position.
+
+
+fruits = ["alu", "bhindi", "karela", "kaddu"]
+
+fruits.insert(2, "khira")
+
+print(fruits)
