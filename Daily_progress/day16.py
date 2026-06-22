5a37410c461115b1b58c25072a1226adec01d355
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-22 22:26:19 +0530

    day 16

---
+++ Daily_progress/day16.py
@@ -0,0 +1,47 @@
+# Remove List Items
+
+
+'''
+
+remove()
+pop()
+del
+
+'''
+
+
+
+# Remove()  =  removes by value
+
+
+players = ["player1", "player2", "player3"]
+players.remove("player2")
+
+print(players)
+
+
+# pop()   =   removes by Index
+
+
+vegetables = ["aalu", "loki", "khira", "mirchi", "kela"]
+
+vegetables.pop(2)
+
+print(vegetables)
+
+
+
+# Special POP   =   if vegetables.pop()     then python removes the last item from the list
+
+
+
+
+
+
+
+# del   =       Deletes using Index
+
+del vegetables[1]
+
+print(vegetables)
+
