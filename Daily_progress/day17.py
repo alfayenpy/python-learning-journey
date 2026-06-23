3337ba6db8659d7c517f59180003636389da9928
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-23 23:09:02 +0530

    day 17

---
+++ Daily_progress/day17.py
@@ -0,0 +1,42 @@
+# Python Loop Lists
+
+
+# For loop
+
+'''
+Take one country
+from countries
+again and again
+until the list finishes.
+'''
+
+
+
+
+countries = ["albania", "osaka", "kyoto", "Japan"]
+
+for country in countries:
+    print(countries)
+
+
+
+
+items = ["toy", "book", "pen", "pencil"]
+
+for item in items:
+    print(item)
+
+
+
+
+# Loop through the Index Number
+
+
+# Range()
+
+
+for i in range(1, 5):
+    print(i)
+
+
+    
