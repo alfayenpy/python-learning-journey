1bd107cc3b00b49790430effb181441dea546db4
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-25 23:17:23 +0530

    day 19

---
+++ Daily_progress/day19.py
@@ -0,0 +1,25 @@
+#      Sort Lists   =   sort()
+
+
+# Python sorts alphabetically.
+
+
+countries = ["Japan", "India", "Canada"]
+
+countries.sort()
+
+print(countries)
+
+
+'''reverse=True'''
+
+countries.sort(reverse=True)
+
+
+
+numbers = [10, 20, 30, 5632, 654]
+
+numbers.sort()
+
+print(numbers)
+
