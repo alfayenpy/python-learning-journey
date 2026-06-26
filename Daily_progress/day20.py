0a9a353bfc2b365ab2dc09d9c8f3aede45146268
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-26 23:46:54 +0530

    day 20

---
+++ Daily_progress/day20.py
@@ -0,0 +1,34 @@
+# Copy Lists
+
+
+countries = ["mumbai", "west bengal", "delhi"]
+
+
+
+
+# 1st Method
+
+new_countries = countries.copy()
+
+print(new_countries)
+
+
+# 2nd Method
+
+
+new_list = list(countries)
+
+
+
+
+
+# Code Practice
+
+countries = ["India", "Japan"]
+
+new_list = countries.copy()
+
+new_list.append("Canada")
+
+print(countries)
+print(new_list)
