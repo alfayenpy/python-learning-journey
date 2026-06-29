c967deea0f166246d1d566e094d07ffc8fee0ae7
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-06-29 23:38:07 +0530

    day 23

---
+++ Daily_progress/day23.py
@@ -0,0 +1,22 @@
+# Python Tuples
+
+'''A tuple is a collection of multiple items, just like a list.
+
+
+Lists = []      ,Can be Changed
+
+Tuples = ()     ,Cannot be Changed
+
+'''
+
+
+countries = ("India", "Japan", "Canada")
+
+print(countries)
+print(countries[2])
+print(countries[0])
+print(countries[-1])
+print(countries[0:2])
+
+
+
