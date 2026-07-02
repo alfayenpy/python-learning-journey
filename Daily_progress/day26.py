6c84b818311e2090108fa3cbe5acfd3327503833
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-07-02 23:38:17 +0530

    day 26

---
+++ Daily_progress/day26.py
@@ -0,0 +1,44 @@
+# Python Sets
+
+'''A Set is a collection of items.
+
+
+List  → [ ]
+
+Tuple → ( )
+
+Set   → { }
+
+
+'''
+
+
+
+
+# No Duplicate Values
+
+countries = {"India", "Japan", "India", "Canada"}
+
+print(countries)
+
+
+
+# Unordered =   order is not fixed
+
+cars = {"ferrari", "lamborghini", "merceedez"}
+
+print(cars)
+
+
+# Cannot Access by Index
+
+'''
+
+
+names = {"sahil", "alfayen", "King"}
+
+print(names[1])     --> Error 
+
+
+'''
+
