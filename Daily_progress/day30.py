e394300aaa986c01528565f497824e9216fe12aa
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-07-06 22:09:34 +0530

    day 30

---
+++ Daily_progress/day30.py
@@ -0,0 +1,54 @@
+# Python Set Methods
+
+
+
+
+# pop() 
+
+numbers = {10, 20, 30}
+
+numbers.pop()
+
+print(numbers)
+
+
+
+# clear()
+
+
+numbers = {10, 20, 30}
+
+numbers.clear()
+
+print(numbers)
+
+
+
+
+# copy()
+
+
+set1 = {"India", "Japan"}
+
+set2 = set1.copy()
+
+print(set2)
+
+
+
+
+
+# len
+myset = {"dsa"}
+
+print(len(myset))
+
+
+
+
+
+# in
+
+print("India" in myset)
+
+
