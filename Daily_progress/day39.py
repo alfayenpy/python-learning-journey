68c631ce15ccc2616c1fe009ad5d3fc5f8859f23
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-07-15 22:11:44 +0530

    day 39

---
+++ Daily_progress/day39.py
@@ -0,0 +1,77 @@
+# Python - Nested Dictionaries
+
+# A dictionary can contain dictionaries, this is called nested dictionaries.
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+
+'''
+
+if you want to add three dictionaries into a new dictionary:
+
+Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
+
+'''
+
+
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+
+
+print()
+
+
+
+
+# Access Items in Nested Dictionaries
+
+
+print(myfamily["child2"]["name"])
+
+
+
+print()
+
+# Loop Through Nested Dictionaries
+
+
+#  You can loop through a dictionary by using the items() method like this:
+
+
+
+
+for x, obj in myfamily.items():
+  print(x)
+
+  for y in obj:
+    print(y + ':', obj[y])
+
