29fa70e18d1cd1e18b475d944d058d44accaeced
MD.ALFAYEN MONDAL <mdalfayenm@gmail.com>
2026-07-17 23:53:06 +0530

    day 41

---
+++ Daily_progress/day41.py
@@ -0,0 +1,77 @@
+# Python If Statement
+
+
+'''
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+'''
+
+
+
+
+# If Statement
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+
+# Checking if a number is positive:
+
+number = 15
+if number > 0:
+  print("The number is positive")
+
+
+
+
+
+# Indentation
+
+
+'''Python relies on indentation (whitespace at the beginning of a line) to define scope in the code. Other programming languages often use curly-brackets for this purpose.
+
+
+
+a = 33
+b = 200
+if b > a:
+print("b is greater than a")''' # you will get an error
+
+
+
+
+
+
+
+
+# Multiple Statements in If Block
+
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+
+
+
+# Using Variables in Conditions
+
+
+# Using a boolean variable:
+
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+
